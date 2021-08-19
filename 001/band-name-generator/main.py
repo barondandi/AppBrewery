0555d8036ef8982780b94e823f5fd6e61f3c0f9c
barondandi <barondandi@gmail.com>
2021-08-19 22:47:36 +0200

    Day1 - Adding Solution comments

--- 001/band-name-generator/main.py
+++ 001/band-name-generator/main.py
@@ -1,3 +1,4 @@
+''' VERSION 1
 #1. Create a greeting for your program.
 print("Welcome to Band Name Generator v1.0")
 #2. Ask the user for the city that they grew up in.
@@ -11,6 +12,21 @@ band_name = city + " " + pet
 #5. Make sure the input cursor shows on a new line, see the example at:
 #   https://band-name-generator-end.appbrewery.repl.run/
 print("Your band name could be " + band_name)
+'''
+
+print("┏━━┓━━━━━━━━━━━━┏┓━━━━━━━━━━━━━━━━━━━━━━━━━┏━━━┓━━━━━━━━━━━━━━━━━━━━━┏┓━━━━━━━━━━━━━━━━┏━━━┓━━┏━━━┓")
+print("┃┏┓┃━━━━━━━━━━━━┃┃━━━━━━━━━━━━━━━━━━━━━━━━━┃┏━┓┃━━━━━━━━━━━━━━━━━━━━┏┛┗┓━━━━━━━━━━━━━━━┃┏━┓┃━━┃┏━┓┃")
+print("┃┗┛┗┓┏━━┓━┏━┓━┏━┛┃━━━━┏━┓━┏━━┓━┏┓┏┓┏━━┓━━━━┃┃━┗┛┏━━┓┏━┓━┏━━┓┏━┓┏━━┓━┗┓┏┛┏━━┓┏━┓━━━━┏┓┏┓┗┛┏┛┃━━┃┃━┃┃")
+print("┃┏━┓┃┗━┓┃━┃┏┓┓┃┏┓┃━━━━┃┏┓┓┗━┓┃━┃┗┛┃┃┏┓┃━━━━┃┃┏━┓┃┏┓┃┃┏┓┓┃┏┓┃┃┏┛┗━┓┃━━┃┃━┃┏┓┃┃┏┛━━━━┃┗┛┃┏━┛┏┛━━┃┃━┃┃")
+print("┃┗━┛┃┃┗┛┗┓┃┃┃┃┃┗┛┃━━━━┃┃┃┃┃┗┛┗┓┃┃┃┃┃┃━┫━━━━┃┗┻━┃┃┃━┫┃┃┃┃┃┃━┫┃┃━┃┗┛┗┓━┃┗┓┃┗┛┃┃┃━━━━━┗┓┏┛┃┃┗━┓┏┓┃┗━┛┃")
+print("┗━━━┛┗━━━┛┗┛┗┛┗━━┛━━━━┗┛┗┛┗━━━┛┗┻┻┛┗━━┛━━━━┗━━━┛┗━━┛┗┛┗┛┗━━┛┗┛━┗━━━┛━┗━┛┗━━┛┗┛━━━━━━┗┛━┗━━━┛┗┛┗━━━┛")
+print("━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━")
+print("━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━")
+city = input("In which city did you grow up?\n")
+pet = input("Can you give me the name of a pet?\n")
+band_name = city + " " + pet
+print("Your band name could be " + band_name)
+
 
 ''' SOLUTION
 print("Welcome to the Band Name Generator.")
