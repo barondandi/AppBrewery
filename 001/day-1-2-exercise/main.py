f428286bcbcf49e2351dbab75dd5146b3f00581c
barondandi <barondandi@gmail.com>
2021-08-19 20:43:53 +0200

    Day1 - Adding Solution comments

--- 001/day-1-2-exercise/main.py
+++ 001/day-1-2-exercise/main.py
@@ -12,3 +12,17 @@ print("String Concatenation is done with the \"+\" sign.")
 print('e.g. print("Hello " + "world")')
 print("New lines can be created with a backslash and n.")
 
+
+''' SOLUTION
+#1. Missing double quotes before the word Day.
+print("Day 1 - String Manipulation")
+
+#2. Outer double quotes changed to single quotes.
+print('String Concatenation is done with the "+" sign.')
+
+#3. Extra indentation removed
+print('e.g. print("Hello " + "world")')
+
+#4. Extra ( in print function removed.
+print("New lines can be created with a backslash and n.")
+'''
