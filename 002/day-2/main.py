dc62e0c09ee40a3283aa881a16f3b0d3b99a4e5e
barondandi <barondandi@gmail.com>
2021-08-20 09:00:29 +0200

    Day 2

--- 002/day-2/main.py
+++ 002/day-2/main.py
@@ -21,4 +21,25 @@ print(123_456)
 True
 False
 
+#Type Errors
+num_char = len(input("What is your name?\n"))
+# print("Your name has " + num_char + " characters.")
+# This gives me this error: TypeError: can only concatenate str (not "int") to str
+print("num_char type is:")
+print(type(num_char))
+new_num_char = str(num_char)
+print("new_num_char type is:")
+print(type(new_num_char))
+print("Your name has " + new_num_char + " characters.")
 
+#Type Checking
+# type()
+
+#Type Conversion (TYPECASTING)
+# str()
+# int()
+# float()
+# bool()
+
+print(70 + float("70.15"))
+print(str(123) + str(456))
