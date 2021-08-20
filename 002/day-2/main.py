6359aeaded336ec8392efe39bc37e07e32b7e2e3
barondandi <barondandi@gmail.com>
2021-08-20 08:45:33 +0200

    Day 2

---
+++ 002/day-2/main.py
@@ -0,0 +1,24 @@
+#Data Types
+
+#String
+"Hello"
+# Subscript: method to pull a character out of a string (we start counting at 0):
+print("Hello"[0])
+print("Hello"[4])
+print("Hello"[len("Hello") - 1])
+print("123" + "456")
+
+#Integer
+123
+print(123 + 456)
+# We can divide long numbers with underscores to make it easier for us to read
+print(123_456)
+
+#Float
+3.14159
+
+#Boolean (start with capital letter and no quotation marks)
+True
+False
+
+
