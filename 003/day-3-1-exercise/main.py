c5ef86d494e355c44bc539ff6b68fc10c4b5f9b9
barondandi <barondandi@gmail.com>
2021-08-21 23:08:47 +0200

    Day 3

--- 003/day-3-1-exercise/main.py
+++ 003/day-3-1-exercise/main.py
@@ -4,3 +4,19 @@ number = int(input("Which number do you want to check? "))
 
 #Write your code below this line 👇
 
+remainder = number % 2
+
+if remainder == 0:
+    print("This is an even number.")
+else:
+    print("This is an odd number.")
+
+
+''' SOLUTION
+#If the number can be divided by 2 with 0 remainder.
+if number % 2 == 0:
+  print("This is an even number.")
+#Otherwise (number cannot be divided by 2 with 0 remainder).
+else:
+  print("This is an odd number.")
+'''
