8d108e24fbf7f9f32df264df097811c3b3b42ce0
barondandi <barondandi@gmail.com>
2021-08-25 20:44:31 +0200

    Day 4

---
+++ 004/day-4-1-exercise/main.py
@@ -0,0 +1,19 @@
+import random
+
+random_int = random.randint(0,1)
+
+if random_int == 0:
+    print ("Tails")
+else:
+    print("Heads")
+
+
+''' SOLUTION
+import random
+
+random_side = random.randint(0, 1)
+if random_side == 1:
+  print("Heads")
+else:
+  print("Tails")
+'''
