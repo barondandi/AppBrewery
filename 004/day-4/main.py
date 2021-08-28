30719f58d722bb12f3336d9c753032cbfa856a12
barondandi <barondandi@gmail.com>
2021-08-28 10:42:15 +0200

    Day 4

--- 004/day-4/main.py
+++ 004/day-4/main.py
@@ -14,3 +14,18 @@ print (random_float * 5)
 
 love_score = random.randint(1, 100)
 print(f"Your Love Score is {love_score}.")
+
+state1 = "Delaware"
+state2 = "Pennsylvania"
+
+states_of_america = ["Delaware", "Pennsylvania", "New Jersey", "Georgia", "Connecticut", "Massachusetts", "Maryland", "South Carolina", "New Hampshire", "Virginia", "New York", "North Carolina", "Rhode Island", "Vermont", "Kentucky", "Tennessee", "Ohio", "Louisiana", "Indiana", "Mississippi", "Illinois", "Alabama", "Maine", "Missouri", "Arkansas", "Michigan", "Florida", "Texas", "Iowa", "Wisconsin", "California", "Minnesota", "Oregon", "Kansas", "West Virginia", "Nevada", "Nebraska", "Colorado", "North Dakota", "South Dakota", "Montana", "Washington", "Idaho", "Wyoming", "Utah", "Oklahoma", "New Mexico", "Arizona", "Alaska", "Hawaii"]
+
+# We define the order by the offset from the beginning
+print(states_of_america[0])
+# Last item of the list is -1 as there is not such a thing as -0
+print(states_of_america[-1])
+
+states_of_america[1] = "Pencilvania"
+print(states_of_america)
+states_of_america.append("Angelaland")
+print(states_of_america)
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # AppBrewery
+
+Code for course ***100 Days of Code - The Complete Python Pro Bootcamp for 2021***
+___(https://www.udemy.com/course/100-days-of-code/)___
