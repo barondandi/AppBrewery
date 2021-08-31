3e8bfa33c705ebba47fceafb9ec30fdc170c0414
barondandi <barondandi@gmail.com>
2021-08-31 15:28:20 +0200

    Day 5

--- 005/day-5/main.py
+++ 005/day-5/main.py
@@ -4,3 +4,11 @@ for fruit in fruits:
   print(fruit)
   print(fruit + " Pie")
 print(fruits)
+
+# For loop with range
+# The last number is NOT included
+for number in range(1, 5):
+  print(number)
+
+for number in range(1, 11, 3):
+  print(number)
