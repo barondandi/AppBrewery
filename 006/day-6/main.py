04a59826c1081b57a5ea0b38d988dc730774ed70
barondandi <barondandi@gmail.com>
2021-09-03 16:00:23 +0200

    Day 6

--- 006/day-6/main.py
+++ 006/day-6/main.py
@@ -8,3 +8,42 @@ def my_function():
     print("Bye")
 
 my_function()
+
+'''
+https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Alone&url=worlds%2Ftutorial_en%2Falone.json
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+turn_left()
+move()
+turn_right()
+move()
+turn_right()
+move()
+turn_right()
+move()
+'''
+
+'''
+Hurdles race
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+def jump():
+    move()
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+
+for jumps in range(1, 7):
+    jump()
+'''
