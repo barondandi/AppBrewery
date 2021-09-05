b4e6b18eb1c656703826b1a1a61129d26e15fde7
barondandi <barondandi@gmail.com>
2021-09-05 15:52:55 +0200

    Day 6

--- 006/day-6/main.py
+++ 006/day-6/main.py
@@ -54,3 +54,27 @@ while number_of_hurdles > 0:
     jump()
     number_of_hurdles -= 1
 '''
+
+''' Hurdle 2
+# https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%202&url=worlds%2Ftutorial_en%2Fhurdle2.json
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+def jump():
+    move()
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+
+while at_goal() == False:
+    jump()
+
+# I could also have used 
+# while not at_goal(): 
+'''
