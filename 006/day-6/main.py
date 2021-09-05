93acca77d6d50a565d1a122c02704504a6f37671
barondandi <barondandi@gmail.com>
2021-09-05 17:48:52 +0200

    Day 6

--- 006/day-6/main.py
+++ 006/day-6/main.py
@@ -134,4 +134,25 @@ while not at_goal():
         move()
     else:
         jump()
+
+# Other option:
+# While front_is_clear()
+#   move()
+'''
+
+''' Escaping the maze
+n&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Maze&url=worlds%2Ftutorial_en%2Fmaze1.json
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+while not at_goal():
+    if right_is_clear():
+        turn_right()
+        move()
+    elif front_is_clear():
+        move()
+    else:
+        turn_left()
 '''
