892f5fde85cfec29d51790e8b2b48dfad0b50ce0
barondandi <barondandi@gmail.com>
2021-09-08 13:00:00 +0200

    Day 7

--- 007/day-7-hangman-1/main.py
+++ 007/day-7-hangman-1/main.py
@@ -5,6 +5,8 @@ word_list = ["aardvark", "baboon", "camel"]
 
 #1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
 chosen_word = word_list[random.randint(0, (len(word_list)) - 1)]
+# There is a more simple way to write this:
+# chosen_word = random.choice(word_list)
 # print(chosen_word)
 
 #2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
@@ -13,8 +15,31 @@ guess = guess_unformatted.lower()
 # print(guess)
 
 #3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
-for n in range(0, (len(chosen_word) - 1)):
-    if chosen_word[n] == guess:
+for letter in range(0, (len(chosen_word) - 1)):
+# There is a more simple way to write this:
+# for letter in chosen_word
+    if chosen_word[letter].lower() == guess:
+# This is the way of setting it lowe case
         print("Right")
     else:
         print("Wrong")
+
+''' SOLUTION
+#Step 1 
+
+word_list = ["aardvark", "baboon", "camel"]
+
+#TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
+import random
+chosen_word = random.choice(word_list)
+
+#TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
+guess = input("Guess a letter: ").lower()
+
+#TODO-3 - Check if the letter the user guessed (guess) is one of the leters in the chosen_word.
+for letter in chosen_word:
+    if letter == guess:
+        print("Right")
+    else:
+        print("Wrong")
+'''
