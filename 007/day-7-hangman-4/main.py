5f7172c1a0ded0e052f90580c9c7968e5aa58a44
barondandi <barondandi@gmail.com>
2021-09-10 12:22:10 +0200

    Day 7

--- 007/day-7-hangman-4/main.py
+++ 007/day-7-hangman-4/main.py
@@ -66,6 +66,7 @@ word_length = len(chosen_word)
 
 #TODO-1: - Create a variable called 'lives' to keep track of the number of lives left.
 #Set 'lives' to equal 6.
+lives = 6
 
 #Testing code
 print(f'Pssst, the solution is {chosen_word}.')
@@ -85,9 +86,11 @@ while not end_of_game:
         if letter == guess:
             display[position] = letter
 
-    #TODO-2: - If guess is not a letter in the chosen_word,
+    #2: - If guess is not a letter in the chosen_word,
     #Then reduce 'lives' by 1.
     #If lives goes down to 0 then the game should stop and it should print "You lose."
+    if not (guess in display):
+        lives -= 1
 
     #Join all the elements in the list and turn it into a String.
     print(f"{' '.join(display)}")
@@ -96,5 +99,9 @@ while not end_of_game:
     if "_" not in display:
         end_of_game = True
         print("You win.")
+    elif lives == 0:
+        end_of_game = True
+        print("You lose.")
 
-    #TODO-3: - print the ASCII art from 'stages' that corresponds to the current number of 'lives' the user has remaining.
+    #3: - print the ASCII art from 'stages' that corresponds to the current number of 'lives' the user has remaining.
+    print(stages[lives])
