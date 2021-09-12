183872f266e6a71aa8042adb93c564127fdd2c3f
barondandi <barondandi@gmail.com>
2021-09-12 17:29:56 +0200

    Day 8

--- 008/day-8/main.py
+++ 008/day-8/main.py
@@ -10,3 +10,9 @@ def greet_with_name(name):
     print(f"Hello {name}.")
     print(f"How do you do {name}?")
 greet_with_name("Victor")
+
+#Functions with more than 1 input
+def greet_with(name, location):
+    print(f"Hello {name}.")
+    print(f"How is the weather in {location}?")
+greet_with("Victor", "Madrid")
