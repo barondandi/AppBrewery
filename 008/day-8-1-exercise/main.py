620181665bb2ef5694dd63f37ffe4efaaf9af0fa
barondandi <barondandi@gmail.com>
2021-09-12 18:16:07 +0200

    Day 8

---
+++ 008/day-8-1-exercise/main.py
@@ -0,0 +1,23 @@
+#Write your code below this line 👇
+from math import ceil
+
+# Two ways of rounding up a number:
+# import math
+# print(int(math.ceil(4.2)))
+# Second way:
+# int(21 / 5) + (21 % 5 > 0)
+# The first part becomes 4 and the second part evaluates to "True" if there is a remainder, which in addition True = 1; False = 0.
+
+def paint_calc(height, width, cover):
+    number_of_cans = ceil((height * width) / cover)
+    print(f"You'll need {number_of_cans} cans of paint.")
+
+
+#Write your code above this line 👆
+# Define a function called paint_calc() so that the code below works.
+
+# 🚨 Don't change the code below 👇
+test_h = int(input("Height of wall: "))
+test_w = int(input("Width of wall: "))
+coverage = 5
+paint_calc(height=test_h, width=test_w, cover=coverage)
--- 008/day-8/main.py
+++ 008/day-8/main.py
@@ -3,18 +3,26 @@ def greet():
     print("This is")
     print("a simple")
     print("function")
+
+
 greet()
 
-#Function that allows for input
+
+# Function that allows for input
 def greet_with_name(name):
     print(f"Hello {name}.")
     print(f"How do you do {name}?")
+
+
 greet_with_name("Victor")
 
-#Functions with more than 1 input
+
+# Functions with more than 1 input
 def greet_with(name, location):
     print(f"Hello {name}.")
     print(f"How is the weather in {location}?")
+
+
 greet_with("Victor", "Madrid")
 # To change the parameters order
-greet_with(location = "Madrid", name = "Victor")
+greet_with(location="Madrid", name="Victor")
