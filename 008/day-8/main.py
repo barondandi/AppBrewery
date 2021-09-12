7f4f747a72ec6266ed77e4459850817d338fb2ee
barondandi <barondandi@gmail.com>
2021-09-12 17:22:06 +0200

    Day 8

---
+++ 008/day-8/main.py
@@ -0,0 +1,12 @@
+# Simple Function
+def greet():
+    print("This is")
+    print("a simple")
+    print("function")
+greet()
+
+#Function that allows for input
+def greet_with_name(name):
+    print(f"Hello {name}.")
+    print(f"How do you do {name}?")
+greet_with_name("Victor")
