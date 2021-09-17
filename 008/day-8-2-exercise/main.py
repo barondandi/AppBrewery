e5d8527f6480589e54d2d842cd0a4b06aaae86b2
barondandi <barondandi@gmail.com>
2021-09-17 11:08:58 +0200

    Day 8

--- 008/day-8-2-exercise/main.py
+++ 008/day-8-2-exercise/main.py
@@ -14,3 +14,15 @@ def prime_checker(number):
 # Do NOT change any of the code below👇
 n = int(input("Check this number: "))
 prime_checker(number=n)
+
+''' SOLUTION
+def prime_checker(number):
+    is_prime = True
+    for i in range(2, number):
+        if number % i == 0:
+            is_prime = False
+    if is_prime:
+        print("It's a prime number.")
+    else:
+        print("It's not a prime number.")
+'''
