f9b99ef0843672aadba80ddc1564a87519b60d21
barondandi <barondandi@gmail.com>
2021-09-25 09:19:00 +0200

    Day 8

--- 008/caesar-cipher-3/main.py
+++ 008/caesar-cipher-3/main.py
@@ -3,24 +3,24 @@ alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n'
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
+alphabet_lenght = len(alphabet)
 
-#TODO-1: Combine the encrypt() and decrypt() functions into a single function called caesar().
+#1: Combine the encrypt() and decrypt() functions into a single function called caesar().
 
 def caesar(input_text, shift_amount, selection):
     output_text = ""
     for letter in input_text:
         position = alphabet.index(letter)
-        if direction == "encode":
+        if selection == "encode":
             new_position = position + shift_amount
-            if new_position > len(alphabet):
-                new_position -= len(alphabet)
-            output_text += alphabet[new_position]
-        elif direction == "decode":
+            if new_position >= alphabet_lenght:
+                new_position -= alphabet_lenght
+        elif selection == "decode":
             new_position = position - shift_amount
             if new_position < 0:
-                new_position += len(alphabet)
-            output_text += alphabet[new_position]
-    print(f"The {direction}d text is {output_text}.")
+                new_position += alphabet_lenght
+        output_text += alphabet[new_position]
+    print(f"The {selection}d text is {output_text}.")
 
-#TODO-2: Call the caesar() function, passing over the 'text', 'shift' and 'direction' values.
+#2: Call the caesar() function, passing over the 'text', 'shift' and 'direction' values.
 caesar(input_text=text, shift_amount=shift, selection=direction)
