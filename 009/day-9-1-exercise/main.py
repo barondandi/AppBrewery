df3601ede1eaedf71603e056f1830d0692497a43
barondandi <barondandi@gmail.com>
2021-09-29 12:17:25 +0200

    Day 9

--- 009/day-9-1-exercise/main.py
+++ 009/day-9-1-exercise/main.py
@@ -7,11 +7,21 @@ student_scores = {
 }
 # 🚨 Don't change the code above 👆
 
-# TODO-1: Create an empty dictionary called student_grades.
+# 1: Create an empty dictionary called student_grades.
+student_grades = {}
 
-
-# TODO-2: Write your code below to add the grades to student_grades.👇
-
+# 2: Write your code below to add the grades to student_grades.👇
+for key in student_scores:
+    grade = ""
+    if student_scores[key] >= 91:
+        grade = "Outstanding"
+    elif student_scores[key] >= 81:
+        grade = "Exceeds Expectations"
+    elif student_scores[key] >= 71:
+        grade = "Acceptable"
+    else:
+        grade = "Fail"
+    student_grades[key] = grade
 
 # 🚨 Don't change the code below 👇
 print(student_grades)
