1d4b59fd9e06fb2b75b41825025b0681c423986c
barondandi <barondandi@gmail.com>
2021-09-29 12:06:17 +0200

    Day 8

--- 009/day-9/main.py
+++ 009/day-9/main.py
@@ -1,7 +1,36 @@
+##Python Dictionaries
+# Dictionaries in Python are used to store set of data like Key: Value pair
+# the syntax of a dictionary in Python is very simple we use {} inside that
+# we define {Key: Value}, to separate multiple values we use','
+
 programming_dictionary = {
     "Bug": "An error in a program that prevents the program from running as expected.",
     "Function": "A piece of code that you can easily call over and over again.",
-    "Loop": "The action of doing something over and over again",
 }
 
-print(programming_dictionary["Bug"])
+#Retrieving items from dictionary
+print(programming_dictionary["Bug"])    # this will print the definition of Bug
+
+#Adding items to dictionary.
+programming_dictionary["Loop"] = "The action of doing something over and over again"
+print(programming_dictionary)
+
+#Create an empty dictionary
+empty_dictionary = {}
+
+#Wipe an existing dictionary
+programming_dictionary = {}
+print(programming_dictionary)
+
+#Edit and item in a dictionary
+programming_dictionary = {
+    "Bug": "An error in a program that prevents the program from running as expected.",
+    "Function": "A piece of code that you can easily call over and over again.",
+}
+programming_dictionary["Bug"] = "a moth in your computer"
+print(programming_dictionary)
+
+#Loop through a dictionary
+for entry in programming_dictionary:
+    print(f"The key is {entry}; and the value is \"{programming_dictionary[entry]}\".")
+
