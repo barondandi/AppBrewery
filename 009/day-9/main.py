5eeccecd814cef21d89fb920e9e71924c1d4ee30
barondandi <barondandi@gmail.com>
2021-10-01 12:04:45 +0200

    Day 9

--- 009/day-9/main.py
+++ 009/day-9/main.py
@@ -36,11 +36,34 @@ for entry in programming_dictionary:
 
 #Nesting
 capitals = {
-    "France" = "Paris",
-    "Germany" = "Berlin",
+    "France": "Paris",
+    "Germany": "Berlin",
 }
 
 #Nesting a list in a dictionary
 travel_log = {
-    "France" = ["Paris", "Lille", "Dijon"],
+    "France": ["Paris", "Lille", "Dijon"],
+    "Germany": ["Berlin", "Hamburg", "Stuttgart"],
 }
+
+#Nesting dictionary in a dictionary
+travel_log_updated = {
+    "France": {"cities_visited": ["Paris", "Lille", "Dijon"], "total_visits": 12},
+    "Germany": {"cities_visited": ["Berlin", "Hamburg", "Stuttgart"], "total_visits": 5},
+}
+print(travel_log_updated)
+
+#Nesting a dictionary in a list
+travel_log_new = [
+    {
+        "country": "France",
+        "cities_visited": ["Paris", "Lille", "Dijon"],
+        "total_visits": 12,
+    },
+    {
+        "country": "Germany",
+        "cities_visited": ["Berlin", "Hamburg", "Stuttgart"],
+        "total_visits": 5,
+    },
+]
+print(travel_log_new)
