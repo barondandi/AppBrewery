57b09b30472e96d9fc07d2f79b404d1d0435e27e
barondandi <barondandi@gmail.com>
2021-10-01 13:08:38 +0200

    Day 9

--- 009/blind-auction/main.py
+++ 009/blind-auction/main.py
@@ -1,3 +1,28 @@
 from replit import clear
 #HINT: You can call clear() to clear the output in the console.
 
+from art import logo
+
+#Clear screen andd print logo
+clear()
+print(logo)
+print("Welcome to the secret auction program.")
+
+#Initialize variables
+bids = {}
+continue_bidding = True
+
+#Define function to add bids
+def add_bid():
+    bid_name = str(input("What is your name?: "))
+    bid_amount = int(input("What's your bid?: $"))
+    bids[bid_name]: bid_amount
+    print(bids)
+    bid_continue = (str(input("Are there any other bidders? Type 'yes' or 'no'.\n"))).lower()
+    if bid_continue == "no":
+        continue_bidding = False
+    clear()
+
+#Main code
+while continue_bidding:
+    add_bid()
