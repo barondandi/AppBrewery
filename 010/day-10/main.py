1dcf9e675b78cedba5baf9186ea69c61fa90c152
barondandi <barondandi@gmail.com>
2021-10-11 10:51:13 +0200

    Day 10

--- 010/day-10/main.py
+++ 010/day-10/main.py
@@ -3,3 +3,16 @@ def my_function():
     return result
 
 print(my_function())
+
+#Functions with Outputs
+def format_name(f_name, l_name):
+    if f_name == "" or l_name == "":
+        return "You didn't provide valid inputs."
+    formatted_f_name = f_name.title()
+    formatted_l_name = l_name.title()
+    #print(f"{formatted_f_name} {formatted_l_name}")
+    return f"{formatted_f_name} {formatted_l_name}"
+    #the return should be the last line of the function. Anything after will not be executed
+
+print(format_name("AnGEla", "YU"))
+print(format_name(input("What is your first name?"), input("What is your last name?")))
