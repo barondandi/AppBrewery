baae6b7bf89d171b7fbcdede3af1fb3882e8b32b
barondandi <barondandi@gmail.com>
2021-10-11 12:47:07 +0200

    Day 10

--- 010/calculator/main.py
+++ 010/calculator/main.py
@@ -27,20 +27,36 @@ operations = {
     "/": divide,
 }
 
+#Function to select operator. Need to define it, so that it can be reused
+def select_operator():
+    operation_symbol_error = True
+    while operation_symbol_error:
+        for key in operations:
+            print(key)
+        operation_symbol = input("Pick an operation from the line above: ")
+        if operation_symbol in operations:
+            operation_symbol_error = False
+            return operation_symbol
+        else:
+            print("Invalid operator.")
+
 num1 = int(input("What's the first number?: "))
-
-operation_symbol_error = True
-while operation_symbol_error:
-    for key in operations:
-        print(key)
-    operation_symbol = input("Pick an operation from the line above: ")
-    if operation_symbol in operations:
-        operation_symbol_error = False
-    else:
-        print("Invalid operator.")
-
+operation_choice = select_operator()
 num2 = int(input("What's the second number?: "))
-first_answer = operations[operation_symbol](num1, num2)
+first_answer = operations[operation_choice](num1, num2)
 
-print(f"{num1} {operation_symbol} {num2} = {first_answer}")
+print(f"{num1} {operation_choice} {num2} = {first_answer}")
 
+#Do another operation with the previous result
+continue_calculations = True
+while continue_calculations:
+    continue_selection = (input(f"Type 'y' to continue calculating with {first_answer} or type 'n' to exit\n")).lower()
+    if continue_selection == "y":
+        print("Pick another operation:")
+        operation_choice = select_operator()
+        num3 = int(input("What's the next number?: "))
+        second_answer = operations[operation_choice](first_answer, num3)
+        print(f"{first_answer} {operation_choice} {num3} = {second_answer}")
+        first_answer = second_answer
+    else:
+        continue_calculations = False
